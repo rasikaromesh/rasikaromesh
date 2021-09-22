479639d610035da1e23e53d1a05adf4a9d521a46
rasikaromesh <rasikaromesh000@gmail.com>
2021-09-22 11:14:54 +0800

    social button and top bar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,6 +1,6 @@
 import "./topbar.scss"
 import { Person, Mail } from "@material-ui/icons";
-// import data from "./topbar.json"
+import SocialButtons from "../common/socialButtons/SocialButtons"
 
 export default function Topbar({menuOpen, setMenuOpen}) {
     // console.log(data);
@@ -8,22 +8,25 @@ export default function Topbar({menuOpen, setMenuOpen}) {
         <div className={"topbar " + (menuOpen && "active")}>
             <div className="wrapper">
                 <div className="left">
-                    <a href="#intro" className="logo">fdsf</a>
+                    <a href="#intro" className="logo">Romesh Dhananjaya</a>
                     <div className="itemContainer">
                         <Person className="icon"/>
-                        <span>flsdf</span>
+                        <span>+65 81226123</span>
                     </div>
                     <div className="itemContainer">
                         <Mail className="icon"/>
-                        <span>fsdlfjldsf</span>
+                        <span>romesh.d.sg@gmail.com</span>
                     </div>
                 </div>
                 <div className="right">
-                <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+                <div>
+                    <SocialButtons/>
+                </div>
+                {/* <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
                     <span className="line1"></span>
                     <span className="line2"></span>
                     <span className="line3"></span>
-                </div>
+                </div> */}
                 </div>
             </div>
         </div>
