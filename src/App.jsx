abbdeb67723b011ef11f693bbcea3636edb2df83
rasikaromesh <rasikaromesh000@gmail.com>
2021-09-23 17:40:54 +0800

    blog and blog post

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
 import Topbar from './components/topbar/Topbar';
-import Intro from './components/intro/Intro';
-import Menu from './components/menu/Menu';
-import Card from './components/common/card/Card';
-import Blog from './components/blog/Blog';
+import BlogPost from './components/blogPost/BlogPost';
+import MainPage from './components/MainPage/MainPage';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 import './app.scss';
-import { useState } from 'react';
 
 function App() {
-  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="app">
-      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Topbar />
+      {/* <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} /> */}
       <div className="section">
-        <Intro />
-        <div className="cardWrapper">
-          <Card />
-          <Blog />
-        </div>
+        <Router>
+          <Switch>
+            <Route path="/post">
+              <BlogPost />
+            </Route>
+            <Route exact path="/">
+              <MainPage />
+            </Route>
+          </Switch>
+        </Router>
       </div>
     </div>
   );
--- src/components/blog/BlogPostCard/BlogPostCard.jsx
+++ src/components/blog/BlogPostCard/BlogPostCard.jsx
@@ -1,8 +1,13 @@
+import { useHistory } from 'react-router';
 import './blogPostCard.scss';
 import Tag from './Tag/Tag';
 export default function BlogPostCard({ date, title, summery, tags }) {
+  let history = useHistory();
+  const onClickHandler = () => {
+    history.push('/post');
+  };
   return (
-    <div className="blog-post-card">
+    <div className="blog-post-card" onClick={onClickHandler}>
       <div className="left">
         <span className="year">{date.year}</span>
         <span className="day">{date.day}</span>
